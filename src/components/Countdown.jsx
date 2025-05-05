91fea4f244345d074c628fcb1d6a87c7716bbee2
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2025-05-05 13:45:51 +0330

    debuging is compeleted

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -2,62 +2,58 @@
  *  2022 Sergi S. - https://github.com/sergiss
  */
 
-import React, { useEffect, useState } from 'react'
-import { Number } from './Number'
-import { Word } from './Word'
+import React, { useEffect, useState } from "react";
+import { Number } from "./Number";
+import { Word } from "./Word";
 
-export const Countdown = ({tgt = 20000, event}) => {
+export const Countdown = ({ tgt = 20000, event }) => {
+  const [millis, setMillis] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
 
-    const [millis , setMillis ] = useState(0);
-    const [seconds, setSeconds] = useState(0);
-    const [hours  , setHours  ] = useState(0);
+  const [time, setTime] = useState(tgt);
 
-    const [time, setTime] = useState(tgt);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((time) => {
+        if (time <= 0) {
+          clearInterval(interval);
+          setMillis(0);
+          setSeconds(0);
+          setHours(0);
+          event();
+          return 0;
+        } else {
+          setMillis(time % 100);
+          setSeconds(Math.floor((time / 1000) % 60));
+          setMinutes(Math.floor(time / 1000 / 60));
+          setHours(Math.floor(time / 1000 / 60 / 60));
+          return time - 60;
+        }
+      });
+    }, 60);
 
-    useEffect(()=> {
+    return (e) => clearInterval(interval);
+  }, []);
 
-        const interval = setInterval(()=> {
+  const addTime = () => {
+    setTime(time + 10000);
+  };
 
-            setTime(time=> {
+  return (
+    <div className="clock">
+      <div className="countdown">
+        <Number value={hours} />
+        <Word value={":"} />
+        <Number value={minutes} />
+        <Word value={":"} />
+        <Number value={seconds} />
+        <Word value={":"} />
+        <Number value={millis} />
+      </div>
 
-                if(time <= 0) {
-                    clearInterval(interval);
-                    setMillis(0);
-                    setSeconds(0);
-                    setHours(0);
-                    event();
-                    return 0;
-                } else {
-                    setMillis(time % 99);
-                    setSeconds(Math.floor((time / 1000) % 60));
-                    setHours(Math.floor(time / 1000 / 60));
-                    return time - 60;
-                }
-                
-            });
-        }, 60);
-
-        return e => clearInterval(interval);
-
-    }, []);
-
-    const addTime = ()=> {
-        setTime(time + 10000);
-    }
-
-    return (
-        <div className='clock'>
-
-            <div className='countdown'>
-                <Number value={hours} />
-                <Word value={':'} />
-                <Number value={seconds} />
-                <Word value={':'} />
-                <Number value={millis} />
-            </div>
-
-            <button onClick={e=>addTime()}>Press to save the world</button>
-       
-        </div>
-    )
-}
+      <button onClick={(e) => addTime()}>Press to save the world</button>
+    </div>
+  );
+};
